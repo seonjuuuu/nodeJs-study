a637b14a0ca85723ac47ae1630a9dea99f2cd5e7
moonseonju <sjmoon@glowmee.com>
2024-10-17 10:37:05 +0900

    [feat] env 설정 및 로컬 주소에서 서버데이터 주소 변경

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require('./route/index');
+require('dotenv').config();
 const cors = require('cors');
-
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', indexRouter);
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo';
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI)
