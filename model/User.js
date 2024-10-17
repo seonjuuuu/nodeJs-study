d27006cfcab937c11544fd8ab1be23c49fb1da95
moonseonju <sjmoon@glowmee.com>
2024-10-17 16:40:16 +0900

    [feat] 오타 수정

--- model/User.js
+++ model/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const jwt = require('jsonwebToken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
